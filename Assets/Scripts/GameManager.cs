8500c60259eb8cd79986bd4b980bba78aa7340c9
bclepf <bernardo.clepf@alunos.unis.edu.br>
2025-04-05 14:48:20 -0300

    Substâncias classificadas e misturas adicionadas

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        
+    }
+    public void ReiniciarCena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
