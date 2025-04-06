1cc1dc0f3a458a32c26e818bd135c3b5b3d66472
bclepf <bernardo.clepf@alunos.unis.edu.br>
2025-04-06 11:58:11 -0300

    Adicionado EasterEgg Césio

--- Assets/Scripts/CauldronScript.cs
+++ Assets/Scripts/CauldronScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,42 +8,53 @@ using UnityEngine.UI;
 public class CauldronScript : MonoBehaviour
 {
     private List<string> ingredientesNoCaldeirao = new List<string>();
-    private const int limiteIngredientes = 3;  
-    private bool estaMisturando = false;        
+    private const int limiteIngredientes = 3;
+    private bool estaMisturando = false;
+
+    private string _raridadeResultado;
+    private string _nomeResultado;
 
     [SerializeField] private ParticleSystem efeitoMistura;
     [SerializeField] private AudioSource somMistura;
     [SerializeField] private GameObject _restartLayer;
     [SerializeField] private GameObject _flasks;
+    [SerializeField] private Image _filtroGradiente;
 
     #region Misturas
-    [SerializeField] private Transform pontoDeSpawn;
-    [SerializeField] private GameObject prefabCesio;
-    [SerializeField] private GameObject prefabGolem;
-    [SerializeField] private GameObject prefabPocaoEstragada;
+    [SerializeField] private Transform _pontoDeSpawn;
+    [SerializeField] private GameObject _prefabCesio;
+    [SerializeField] private GameObject _prefabGolem;
+    [SerializeField] private GameObject _prefabFalho;
 
     private Dictionary<string, GameObject> mapaDePrefabs = new Dictionary<string, GameObject>();
     private Dictionary<string, (string, string)> receitas = new Dictionary<string, (string, string)>
     {
         { "Ácido+Energia+Tritio", ("Césio", "Lendária") },
         { "Energia+Solução de Ferro+Vida Líquida", ("Golem", "Rara") },
-        
+
     };
     #endregion
 
+    #region Cesio
 
+    [SerializeField] private Transform _cesioPosition;
+    [SerializeField] private AudioClip _cesioSound;
+    [SerializeField] private Color _corInicialCesio;
+    [SerializeField] private Color _corFinalCesio;
+
+    #endregion
 
     private void Start()
     {
-        mapaDePrefabs["Césio"] = prefabCesio;
-        mapaDePrefabs["Golem"] = prefabGolem;
-        mapaDePrefabs["Poção Estragada"] = prefabPocaoEstragada;
+        mapaDePrefabs["Césio"] = _prefabCesio;
+        mapaDePrefabs["Golem"] = _prefabGolem;
+        mapaDePrefabs["Mistura Falha"] = _prefabFalho;
     }
 
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))  
+        if (Input.GetKeyDown(KeyCode.R))
         {
             GameManager.instance.ReiniciarCena();
         }
@@ -55,22 +67,22 @@ public class CauldronScript : MonoBehaviour
     #region Misturas
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (estaMisturando) return;  
+        if (estaMisturando) return;
 
         FlaskScript pocao = other.GetComponent<FlaskScript>();
 
         if (pocao != null)
         {
             string nomePocao = pocao.GetNomePocao();
-            if (!ingredientesNoCaldeirao.Contains(nomePocao))  
+            if (!ingredientesNoCaldeirao.Contains(nomePocao))
             {
-                if (ingredientesNoCaldeirao.Count < limiteIngredientes)  
+                if (ingredientesNoCaldeirao.Count < limiteIngredientes)
                 {
                     ingredientesNoCaldeirao.Add(nomePocao);
-                    Debug.Log($"Poção adicionada: {nomePocao}");  
-                    Destroy(other.gameObject);  
+                    Debug.Log($"Poção adicionada: {nomePocao}");
+                    Destroy(other.gameObject);
 
-                    if (ingredientesNoCaldeirao.Count == limiteIngredientes)  
+                    if (ingredientesNoCaldeirao.Count == limiteIngredientes)
                     {
                         StartCoroutine(ProcessarMistura());
                     }
@@ -89,16 +101,16 @@ public class CauldronScript : MonoBehaviour
 
     private IEnumerator ProcessarMistura()
     {
-        estaMisturando = true;  
+        estaMisturando = true;
 
         if (efeitoMistura != null) efeitoMistura.Play();
         if (somMistura != null) somMistura.Play();
 
         Debug.Log("Misturando...");
 
-        yield return new WaitForSeconds(2f);  
+        yield return new WaitForSeconds(2f);
 
-        Misturar();  
+        Misturar();
         estaMisturando = false;
 
     }
@@ -108,38 +120,77 @@ public class CauldronScript : MonoBehaviour
         ingredientesNoCaldeirao.Sort();
         string combinacao = string.Join("+", ingredientesNoCaldeirao);
 
-        string nomeResultado;
-
         if (receitas.ContainsKey(combinacao))
         {
-            nomeResultado = receitas[combinacao].Item1;
-            string raridadeResultado = receitas[combinacao].Item2;
+            _nomeResultado = receitas[combinacao].Item1;
+            _raridadeResultado = receitas[combinacao].Item2;
 
-            Debug.Log($"Mistura feita! Você criou uma {nomeResultado} ({raridadeResultado})");
+            if (_raridadeResultado != "Lendária")
+            {
+                Debug.Log($"Mistura feita! Você criou uma {_nomeResultado} ({_raridadeResultado})");
+                SummonResult();
+            }
+            else
+            {
+                SummonEasterEgg(_nomeResultado);
+            }
         }
         else
         {
-            nomeResultado = "Poção Estragada";
-            Debug.Log("A mistura falhou! Você criou uma Poção Estragada!");
+            _nomeResultado = "Mistura Falha";
+            Debug.Log("Sua mistura resultou e um montuado de poeira");
+            SummonResult();
         }
+    }
 
-        
-
+    private void SummonResult()
+    {
         // Spawna o resultado
-        if (mapaDePrefabs.TryGetValue(nomeResultado, out GameObject prefab))
+        if (mapaDePrefabs.TryGetValue(_nomeResultado, out GameObject prefab))
         {
-            Instantiate(prefab, pontoDeSpawn.position, Quaternion.identity);
+            Instantiate(prefab, _pontoDeSpawn.position, Quaternion.identity);
         }
         else
         {
-            Debug.LogWarning("Prefab não encontrado para: " + nomeResultado);
+            Debug.LogWarning("Prefab não encontrado para: " + _nomeResultado);
         }
 
         StartCoroutine(ResetGame());
-        
     }
 
-    private IEnumerator ResetGame() 
+
+
+    #region EasterEggs
+
+    private void SummonEasterEgg(string easteregg)
+    {
+        switch (easteregg)
+        {
+            case "Césio":
+                if (_cesioSound != null)
+                    AudioSource.PlayClipAtPoint(_cesioSound, transform.position);
+                StartCoroutine(TrocarCorFundo(_corInicialCesio, _corFinalCesio));
+
+                if (mapaDePrefabs.TryGetValue("Césio", out GameObject prefabCesio))
+                {
+                    Instantiate(prefabCesio, _cesioPosition.position, Quaternion.identity);
+                }
+
+                Debug.Log("EasterEgg Césio");
+                break;
+
+            default:
+                Debug.LogWarning($"Easter egg desconhecido: {easteregg}. Invocando normalmente...");
+                SummonResult();
+                break;
+        }
+
+        StartCoroutine(ResetGame());
+    }
+
+    #endregion
+
+    private IEnumerator ResetGame()
     {
         yield return new WaitForSeconds(3f);
 
@@ -147,6 +198,23 @@ public class CauldronScript : MonoBehaviour
         ingredientesNoCaldeirao.Clear();
         _restartLayer.SetActive(true);
         _flasks.SetActive(false);
+    }
+
+    private IEnumerator TrocarCorFundo(Color corInicial, Color corFinal)
+    {
+        Debug.LogWarning("Iniciando Troca de cor de fundo");
+        float duracao = 8f;
+        float tempo = 0f;
+
+        while (tempo < duracao)
+        {
+            tempo += Time.deltaTime;
+            float t = tempo / duracao;
+            _filtroGradiente.color = Color.Lerp(corInicial, corFinal, t);
+            yield return null;
+        }
+
+        _filtroGradiente.color = corFinal;
     }
     #endregion
 }
